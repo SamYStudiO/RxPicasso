9b356aea52841e7a9a93ffb3c2d225f0dda64b88
samystudio <contact@samystudio.net>
2018-10-11 23:56:01 +0200

    Use lambdas with Java

--- rxpicasso-sample/src/main/java/net/samystudio/rxpicasso/sample/MainActivityJava.java
+++ rxpicasso-sample/src/main/java/net/samystudio/rxpicasso/sample/MainActivityJava.java
@@ -1,7 +1,6 @@
 package net.samystudio.rxpicasso.sample;
 
 import android.annotation.SuppressLint;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -11,10 +10,6 @@ import net.samystudio.rxpicasso.RxPicasso;
 import net.samystudio.rxpicasso.TargetState;
 
 import androidx.appcompat.app.AppCompatActivity;
-import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Action;
-import io.reactivex.functions.Consumer;
 
 @SuppressWarnings("ResultOfMethodCallIgnored")
 @SuppressLint("CheckResult")
@@ -31,111 +26,51 @@ public class MainActivityJava extends AppCompatActivity {
         // load image into an imageView
         RxPicasso
                 .observeInto(picasso, picasso.load(path), imageView)
-                .subscribe(new Action() {
-                    @Override
-                    public void run() {
-                        // image loaded
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) {
-                        throwable.printStackTrace();
-                    }
-                });
+                .subscribe(() -> {
+                    // image loaded
+                }, Throwable::printStackTrace);
 
         // load image into remote view (notification)
         /*RxPicasso
                 .observeInto(picasso, picasso.load(path), remoteViews, viewId, notficationId, notification, notificationTag)
-                .subscribe(new Action() {
-                    @Override
-                    public void run() throws Exception {
-                        // image loaded
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) {
-                        throwable.printStackTrace();
-                    }
-                });
+                .subscribe(() -> {
+                    // image loaded
+                }, Throwable::printStackTrace);
 
         // load image into remote view (notification)
         RxPicasso
                 .observeInto(picasso, picasso.load(path), remoteViews, viewId, appWidgetIds)
-                .subscribe(new Action() {
-                    @Override
-                    public void run() throws Exception {
-                        // image loaded
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) {
-                        throwable.printStackTrace();
-                    }
-                });*/
+                .subscribe(() -> {
+                    // image loaded
+                }, Throwable::printStackTrace);*/
 
         // load image into a bitmap
         RxPicasso
                 .observeIntoBitmap(picasso, picasso.load(path))
-                .subscribe(new Consumer<Bitmap>() {
-                    @Override
-                    public void accept(Bitmap bitmap) {
-                        // bitmap loaded
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) {
-                        throwable.printStackTrace();
-                    }
-                });
+                .subscribe(bitmap -> {
+                    // bitmap loaded
+                }, Throwable::printStackTrace);
 
         // load image into a Picasso Target
         RxPicasso
                 .observeIntoTarget(picasso, picasso.load(path))
-                .subscribe(new Observer<TargetState>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-
+                .subscribe(targetState -> {
+                    if (targetState instanceof TargetState.PrepareLoad) {
+                        // do something with targetState.placeHolderDrawable
                     }
-
-                    @Override
-                    public void onNext(TargetState targetState) {
-                        if (targetState instanceof TargetState.PrepareLoad) {
-                            // do something with targetState.placeHolderDrawable
-                        }
-
-                        if (targetState instanceof TargetState.BitmapFailed) {
-                            // do something with targetState.errorDrawable
-                        }
-
-                        if (targetState instanceof TargetState.BitmapLoaded) {
-                            // do something with targetState.bitmap or targetState.from
-                        }
+                    if (targetState instanceof TargetState.BitmapFailed) {
+                        // do something with targetState.errorDrawable
                     }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        e.printStackTrace();
+                    if (targetState instanceof TargetState.BitmapLoaded) {
+                        // do something with targetState.bitmap or targetState.from
                     }
-
-                    @Override
-                    public void onComplete() {
-                        // completed
-                    }
-                });
+                }, Throwable::printStackTrace, () -> { /*completed*/ });
 
         // fetch image into cache
         RxPicasso
                 .observeFetch(picasso, picasso.load(path))
-                .subscribe(new Action() {
-                    @Override
-                    public void run() {
-                        // image loaded into cache
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) {
-                        throwable.printStackTrace();
-                    }
-                });
+                .subscribe(() -> {
+                    // image loaded into cache
+                }, Throwable::printStackTrace);
     }
 }
